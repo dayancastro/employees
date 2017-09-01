ce65b7a7e643d3de27ce163212fecf8617db15bb
Dayan <dcterageo@gmail.com>
2017-09-01 10:01:49 -0300

    novas alteracoes

--- client/components/employee_detail.js
+++ client/components/employee_detail.js
@@ -1,11 +1,20 @@
 import React from 'react';
 
-const EmployeeDetail = (props) => {
+const EmployeeDetail = ({ employee }) => {
     //props.employee is our employee model
-
+    // const { name, email, phone, avatar } = employee;
     return (
         <div className="thumbnail"> 
-            <img src={props.employee.avatar}/>
+            <img src={employee.avatar}/>
+            <div className="caption">
+                <h3>{employee.name}</h3>
+                <ul className="list-group">
+                    <li className="list-group-item">Email: {employee.email}</li>
+                    <li className="list-group-item">Phone: {employee.phone}</li>
+                    <li className="list-group-item">Lat: {employee.latitude}</li>
+                    <li className="list-group-item">Long: {employee.longitude}</li>
+                </ul>
+            </div>
         </div>
     );
 };
